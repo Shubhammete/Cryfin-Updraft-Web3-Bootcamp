b1e7f6b89a64dee407a2cff130b6586ca3ebce18
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2024-02-27 15:33:59 +0530

    functions and contracts in solidity

---
+++ Solidity/02_function_and_contract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// contact is like a class in solidity where all data and functions reside
+contract SimpleStorage{
+    uint public favno;
+
+    // function here is public so to access by external body or can be interacted
+    function setfavno(uint _favno) public{
+        favno = _favno;
+    }   
+}
