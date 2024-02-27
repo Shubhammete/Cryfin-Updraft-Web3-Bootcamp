eba32916bea4f7d11f6a0dff8ea20a7520fbd93f
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2024-02-27 15:31:24 +0530

    Data types in solidity

---
+++ Solidity/basics.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+// pragma solidity >=0.8.20 <0.9.0;
+
+contract SimpleStorage{
+    // data types
+    // public here is to show the data to transcation by default it is at internal mode
+    uint internal privateData;
+    uint public myUint;
+    bool public myBool;
+    string public myString;
+    byte public myByte;
+
+}
