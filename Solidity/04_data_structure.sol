0561fa134fdb3fa33440befeb3fae6dc2d2e6c1b
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2024-02-27 19:21:25 +0530

    mapping

--- Solidity/04_data_structure.sol
+++ Solidity/04_data_structure.sol
@@ -24,4 +24,10 @@ contract SimpleStorage{
     function addpeople(uint _no, string memory _name) public{
         listofPeople.push(Person(_no,_name));
     }
+
+    // memory is temporary variable which can be modified and is deleted after function call is executed
+    //  calldata - constant variable which cannot be modified and is same as memory
+    //   storage - variables that are declared outside of the functions but inside contracts they remain even after execution but not deleted after execution of the function btw we cannot pass it in args but used to create objs inside function
+    
+
 }
---
+++ Solidity/05_mapping.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract SimpleStorage{
+    uint public favno;
+
+// map
+    mapping (string => uint) public map;
+     
+ function addpeople(uint _no, string memory _name) public{
+        map[_name] = _no;
+    }
+    
+}
