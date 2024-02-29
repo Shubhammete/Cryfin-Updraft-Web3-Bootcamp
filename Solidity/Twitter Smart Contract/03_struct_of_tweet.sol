61b1053eec897f92ecb078bca9ebeae8e14500e9
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2024-02-29 19:24:39 +0530

    Tweet data in struct i.e. Tweet API JSON format

--- Solidity/Twitter Smart Contract/03_struct_of_tweet.sol
+++ Solidity/Twitter Smart Contract/03_struct_of_tweet.sol
@@ -2,7 +2,38 @@
 pragma solidity  ^0.8.0;
 
 contract demo{
+
+    // twitter metadata
     struct Tweet{
-        
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
     }
+
+    // map to store tweets to the corresponding user  
+    mapping(address => Tweet[]) tweets;
+    
+    // function to create tweet and add to object
+    function createTweet(string memory _content) public {
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _content,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+        tweets[msg.sender].push(newTweet);
+    }
+
+    // function to get the tweet for corresponding user
+    function showTweet(address owner,uint i) public view returns(Tweet memory){
+        return tweets[owner][i];
+    }
+
+    // function to get the tweet array for specific user
+
+    function getTweet(address owner)public view returns (Tweet[] memory){
+        return tweets[owner];
+}
 }
